c471238b20431dfe4891b3df229d98d7b6a89ffc
Color-Kat <katik7979@gmail.com>
2023-06-15 20:45:56 +0300

    right redux

--- resources/js/pages/HomePage/HomePage.tsx
+++ resources/js/pages/HomePage/HomePage.tsx
@@ -1,10 +1,16 @@
-import {Page} from '@modules/PageTemplates';
+import Page from '@/components/PageTemplates/Page';
+import {Helmet} from "react-helmet";
 
 export const HomePage = () => {
     return (
         <Page
             title="Hey, are you my master?"
         >
+            <Helmet>
+                <title>Главная</title>
+                <link rel="canonical" href="http://127.0.0.1:8000" />
+            </Helmet>
+
             <div className="p-10">
                 <div className="animate-slide-up ">
                     Hello, new project ;)
