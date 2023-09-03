2681b2074a670a17846568b146996d996caa8429
Color-Kat <katik7979@gmail.com>
2023-09-03 18:15:36 +0300

    feat(wallets): wallet + wallet history models and migrations

---
+++ database/migrations/2023_09_03_174113_create_wallets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->float('balance', 10, 2)->default(0);
+            $table->float('total_balance', 10, 2)->default(0);
+
+            $table->string('currency')->default('RUB');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
