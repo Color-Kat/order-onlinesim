1b1eb18b760bc7a65252b620db5dd12e3de57bf8
Color-Kat <katik7979@gmail.com>
2023-09-03 18:16:25 +0300

    feat(wallets): WalletObserver for listening wallet history and save it into wallet_histories

---
+++ app/Observers/WalletObserver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Wallet;
+use App\Models\WalletHistory;
+
+class WalletObserver
+{
+    /**
+     * Handle the Wallet "created" event.
+     *
+     * @param  \App\Models\Wallet  $wallet
+     * @return void
+     */
+    public function created(Wallet $wallet)
+    {
+        //
+    }
+
+    /**
+     * Handle the Wallet "updated" event.
+     * Insert new wallet history record when Wallet model is updated
+     *
+     * @param  \App\Models\Wallet  $wallet
+     * @return void
+     */
+    public function updated(Wallet $wallet)
+    {
+        if ($wallet->isDirty('balance')) {
+            $oldBalance = $wallet->getOriginal('balance');
+            $newBalance = $wallet->balance;
+
+            $balanceDiff = $newBalance - $oldBalance;
+
+            WalletHistory::query()->insert([
+                'user_id' => $wallet->user_id,
+                'wallet_id' => $wallet->id,
+                'amount' => $balanceDiff,
+                'currency' => $wallet->currency,
+            ]);
+        }
+    }
+
+    /**
+     * Handle the Wallet "deleted" event.
+     *
+     * @param  \App\Models\Wallet  $wallet
+     * @return void
+     */
+    public function deleted(Wallet $wallet)
+    {
+        //
+    }
+
+    /**
+     * Handle the Wallet "restored" event.
+     *
+     * @param  \App\Models\Wallet  $wallet
+     * @return void
+     */
+    public function restored(Wallet $wallet)
+    {
+        //
+    }
+
+    /**
+     * Handle the Wallet "force deleted" event.
+     *
+     * @param  \App\Models\Wallet  $wallet
+     * @return void
+     */
+    public function forceDeleted(Wallet $wallet)
+    {
+        //
+    }
+}
